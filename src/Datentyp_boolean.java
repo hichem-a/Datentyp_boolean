3363dfec081de59832d785ccab13006f5e979822
hichem-a <hichemabdennadher5@gmail.com>
2023-10-23 14:15:54 +0200

    test

--- src/Datentyp_boolean.java
+++ src/Datentyp_boolean.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Datentyp_boolean {
+public class Datentyp_boolean { //g
     public static void main(String[] args) {
         boolean schalterAn, lampeAn;
         Scanner input = new Scanner(System.in);
