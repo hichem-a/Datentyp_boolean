d1502b4f4f68a9bd88709f29d7706f86ba3df8e3
hichem-a <hichemabdennadher5@gmail.com>
2023-10-27 11:17:45 +0200

    test

--- src/Datentyp_boolean.java
+++ src/Datentyp_boolean.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
 public class Datentyp_boolean {
     public static void main(String[] args) {
-        boolean schalterAn, lampeAn;
+        boolean sAn1,sAn2;
+
         Scanner input = new Scanner(System.in);
-        System.out.print("Ist der Schalter an? (true/false): ");
-        schalterAn = input.nextBoolean();
-        lampeAn = schalterAn;
-        System.out.println("Schalter 1 ist an? " + schalterAn);
-        System.out.println("Das Licht ist an? " + lampeAn);
+
+        System.out.print("Ist der Schalter  1 an? (true/false): ");
+        sAn1 = input.nextBoolean();
+        System.out.print("Ist der Schalter  2an? (true/false): ");
+        sAn2 = input.nextBoolean();
+        System.out.println("Schalter 1 ist an? " + sAn1);
+        System.out.println("Schalter 2 ist an? " + sAn2);
+        if ( !sAn1 || !sAn2 ){
+            System.out.println("Das Licht ist aus ");
+        }
+    else{
+            System.out.println("Das Licht ist an? " );
+
+        }
 input.close();
     }
 }
